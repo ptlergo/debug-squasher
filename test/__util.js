8d8b32abe5ab996fd1a2211481d694083b7e0812
ptlergo <ptlergo@gmail.com>
2016-06-17 19:46:42 -0400

    unit test and code coverage packages

---
+++ test/__util.js
@@ -0,0 +1,9 @@
+const util = require('../src/util');
+const expect = require('chai').expect;
+
+
+describe('Debug-Squasher Debug Tool Test: ', () => {
+  it('Should check util.js file', (done) => {
+    done();
+  });
+});
