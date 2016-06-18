593a842217a63fb31793c0f636b271a5818ae2de
ptlergo <ptlergo@gmail.com>
2016-06-17 20:24:13 -0400

    mocha test success

--- test/__util.js
+++ test/__util.js
@@ -1,9 +1,9 @@
 const util = require('../src/util');
 const expect = require('chai').expect;
 
-
 describe('Debug-Squasher Debug Tool Test: ', () => {
-  it('Should check util.js file', (done) => {
+  it('Should be able to recognize the debug string', (done) => {
+    expect(util.output).to.not.equal(null);
     done();
   });
 });
