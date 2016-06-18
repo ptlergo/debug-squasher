1c93000be6516a6ad047a4e5620e8815611f2017
ptlergo <ptlergo@gmail.com>
2016-06-17 20:49:27 -0400

    code comments

--- src/util.js
+++ src/util.js
@@ -1,8 +1,8 @@
 exports.debug = (title, obj) => {
   const seperator = '\n==========================\n';
-
+  // create output string from variables
   const output = seperator + title + seperator;
-
+  // if DEBUG=true display the debugger
   if (process.env.DEBUG) {
     console.log(output, obj);
   }
--- test/__util.js
+++ test/__util.js
@@ -2,6 +2,7 @@ const util = require('../src/util');
 const expect = require('chai').expect;
 
 describe('Debug-Squasher Debug Tool Test: ', () => {
+  // stub to test output
   it('Should be able to recognize the debug string', (done) => {
     expect(util.output).to.not.equal(null);
     done();
