1fc957ae545654a0babe04395c44a8611f13bf00
ptlergo <ptlergo@gmail.com>
2016-06-21 19:05:00 -0400

    fixed log output format

--- src/util.js
+++ src/util.js
@@ -2,15 +2,16 @@ exports.debug = (title, obj) => {
   const fs = require('fs');
   const tstamp = new Date();
 
+  const timeOutput = ' [ ' + tstamp + ' ]';
   const seperator = '\n==========================\n';
   // create output string from variables
-  const output = seperator + title + seperator + ' [ ' + tstamp + ' ].';
+  const output = seperator + title + seperator;
 
   // if DEBUG=true display the debugger
   if (process.env.DEBUG) {
     fs.appendFile('log/lincoln.log', output, (err) => {
       if (err) throw err;
-      console.log(output);
+      console.log(output, obj, timeOutput);
     });
   }
 };
