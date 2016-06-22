88c9902edb3631eb01ab3ccfb9b8ab9337defb76
Kevin Tucker <kwtucker4@gmail.com>
2016-06-22 13:34:57 -0400

    deleted the commented out old code

--- src/util.js
+++ src/util.js
@@ -1,24 +1,3 @@
-// exports.debug = (title, obj) => {
-//   const fs = require('fs');
-//   // create colors inside conosole.log
-//   const colors = require('colors');
-//   const tstamp = new Date();
-//
-//   const timeOutput = '\n [ ' + tstamp + ' ] \n';
-//   const seperator = '\n==========================\n'.red;
-//   // create output string from variables
-//   const output = seperator + title + seperator;
-//
-//   // if DEBUG=true display the debugger
-//   if (process.env.DEBUG) {
-//     fs.appendFile('log/lincoln.log', output, (err) => {
-//       if (err) throw err;
-//       console.log(output, obj, timeOutput.green);
-//     });
-//   }
-// };
-
-
 exports.debug = (title, obj, method) => {
   const fs = require('fs');
   // create colors inside conosole.log
